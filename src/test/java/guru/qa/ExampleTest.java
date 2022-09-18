b7da22aa0aa515355738cbdc88c045db0e4c4b26
Сергей Квин <lorancem@mail.ru>
2022-09-18 23:28:51 +0300

    fix logic

--- src/test/java/guru/qa/ExampleTest.java
+++ src/test/java/guru/qa/ExampleTest.java
@@ -6,13 +6,13 @@ public class ExampleTest {
 
     @Test
     void exampleTest0() {
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(2>=1);
 
     }
 
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(3 > 2);
+        Assertions.assertTrue(3 >= 2);
 
     }
 
